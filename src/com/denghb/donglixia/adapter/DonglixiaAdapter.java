45c262cbf218f0426496112881e0347c01794f35
dendnight <dendnight@gmail.com>
2015-01-15 19:03:43 +0800

    1

--- src/com/denghb/donglixia/adapter/DonglixiaAdapter.java
+++ src/com/denghb/donglixia/adapter/DonglixiaAdapter.java
@@ -2,10 +2,6 @@ package com.denghb.donglixia.adapter;
 
 import java.util.List;
 
-import com.denghb.donglixia.R;
-import com.denghb.donglixia.model.Donglixia;
-import com.denghb.donglixia.tools.bitmap.ImageWorker;
-
 import android.content.Context;
 import android.support.v4.widget.StaggeredGridView.LayoutParams;
 import android.view.LayoutInflater;
@@ -13,32 +9,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.denghb.donglixia.R;
+import com.denghb.donglixia.model.Donglixia;
+import com.denghb.donglixia.tools.bitmap.ImageWorker;
 
 /**
  * 
  * @author denghb
- *
+ * 
  */
 public class DonglixiaAdapter extends BaseAdapter {
 
-	private Context context;
-    private LayoutInflater mLayoutInflater;
-	private ImageWorker mImageFetcher;
-	
-	private List<Donglixia> list;
-	
-	public DonglixiaAdapter(Context context,ImageWorker mImageFetcher,List<Donglixia> list) {
+	private final Context context;
+	private final LayoutInflater mLayoutInflater;
+	private final ImageWorker mImageFetcher;
+
+	private final List<Donglixia> list;
+
+	public DonglixiaAdapter(Context context, ImageWorker mImageFetcher, List<Donglixia> list) {
 		super();
 		this.context = context;
+		this.mLayoutInflater = LayoutInflater.from(context);
 		this.mImageFetcher = mImageFetcher;
 		this.list = list;
 	}
 
 	@Override
 	public int getCount() {
-		return null == list?0:list.size();
+		return null == list ? 0 : list.size();
 	}
 
 	@Override
@@ -58,36 +58,34 @@ public class DonglixiaAdapter extends BaseAdapter {
 		Donglixia donglixia = list.get(position);
 
 		ViewHolder viewholder = null;
-		if(null == convertView)
-		{
-			convertView = mLayoutInflater.inflate(R.layout.donglixia_item, parent);
-			
+		if (null == convertView) {
+			convertView = mLayoutInflater.inflate(R.layout.donglixia_item, null);
+
 			viewholder = new ViewHolder();
-			viewholder.imageView = (ImageView)convertView.findViewById(R.id.donglixia_image);
-			viewholder.tagView = (TextView)convertView.findViewById(R.id.donglixia_tag);
-            
+			viewholder.imageView = (ImageView) convertView.findViewById(R.id.donglixia_image);
+			viewholder.tagView = (TextView) convertView.findViewById(R.id.donglixia_tag);
+
 			convertView.setTag(viewholder);
-		}else{
+		} else {
 			viewholder = (ViewHolder) convertView.getTag();
 		}
 		// 处理图片
-		//viewholder.imageView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ));
+		// viewholder.imageView.setLayoutParams(new
+		// LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ));
 
 		viewholder.tagView.setText(donglixia.getTag());
-        mImageFetcher.loadImage(donglixia.getUrl(), viewholder.imageView);
-        
+		mImageFetcher.loadImage(donglixia.getUrl(), viewholder.imageView);
 
-        LayoutParams lp = new LayoutParams(convertView.getLayoutParams());
-        lp.span = 3;
-        convertView.setLayoutParams(lp);
+		LayoutParams lp = new LayoutParams(convertView.getLayoutParams());
+		lp.span = 3;
+		convertView.setLayoutParams(lp);
 		return convertView;
 	}
-	
-	
+
 	// *** 内容 ***
 	private class ViewHolder {
-        ImageView imageView;
-        TextView tagView;
-    }
+		ImageView imageView;
+		TextView tagView;
+	}
 
 }
--- src/com/denghb/donglixia/view/MainActivity.java
+++ src/com/denghb/donglixia/view/MainActivity.java
@@ -9,13 +9,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.denghb.donglixia.R;
-import com.denghb.donglixia.adapter.DonglixiaAdapter;
-import com.denghb.donglixia.model.Donglixia;
-import com.denghb.donglixia.tools.Helper;
-import com.denghb.donglixia.tools.bitmap.ImageFetcher;
-import com.denghb.donglixia.tools.bitmap.ImageWorker;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -23,6 +16,7 @@ import android.os.Bundle;
 import android.support.v4.widget.StaggeredGridView;
 import android.support.v4.widget.StaggeredGridView.LayoutParams;
 import android.support.v4.widget.StaggeredGridView.OnScrollListener;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -30,15 +24,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-public class MainActivity extends Activity
-{
+import com.denghb.donglixia.R;
+import com.denghb.donglixia.adapter.DonglixiaAdapter;
+import com.denghb.donglixia.model.Donglixia;
+import com.denghb.donglixia.tools.Helper;
+import com.denghb.donglixia.tools.bitmap.ImageFetcher;
+import com.denghb.donglixia.tools.bitmap.ImageWorker;
+
+public class MainActivity extends Activity {
 	private final static String TAG = MainActivity.class.getSimpleName();
-    private StaggeredGridView mSGV;
-//    private SGVAdapter mAdapter;
-    private DonglixiaAdapter mAdapter;
-    
-    private OnScrollListener mScrollListener = new OnScrollListener() {
-    	
+	private StaggeredGridView mSGV;
+	// private SGVAdapter mAdapter;
+	private DonglixiaAdapter mAdapter;
+
+	private final OnScrollListener mScrollListener = new OnScrollListener() {
+
 		@Override
 		public void onScrollStateChanged(ViewGroup view, int scrollState) {
 			Log.d(TAG, "[onScrollStateChanged] scrollState:" + scrollState);
@@ -46,11 +46,11 @@ public class MainActivity extends Activity
 			case SCROLL_STATE_IDLE:
 				setTitle("SCROLL_STATE_IDLE");
 				break;
-				
+
 			case SCROLL_STATE_FLING:
 				setTitle("SCROLL_STATE_FLING");
 				break;
-				
+
 			case SCROLL_STATE_TOUCH_SCROLL:
 				setTitle("SCROLL_STATE_TOUCH_SCROLL");
 				break;
@@ -62,112 +62,105 @@ public class MainActivity extends Activity
 
 		@Override
 		public void onScroll(ViewGroup view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-			Log.d(TAG, "[onScroll] firstVisibleItem:" + firstVisibleItem + " visibleItemCount:"+visibleItemCount + " totalItemCount:" + totalItemCount);
-			
+			Log.d(TAG, "[onScroll] firstVisibleItem:" + firstVisibleItem + " visibleItemCount:" + visibleItemCount
+					+ " totalItemCount:" + totalItemCount);
+
 		}
 	};
-	private List<Donglixia> list = new ArrayList<Donglixia>();
+	private final List<Donglixia> list = new ArrayList<Donglixia>();
 	private ImageWorker mImageFetcher;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
 		mImageFetcher = new ImageFetcher(this, 240);
 		mImageFetcher.setLoadingImage(R.drawable.empty_photo);
-//        mAdapter = new SGVAdapter(this);
-        mAdapter = new DonglixiaAdapter(this, mImageFetcher, list );
-        
-        mSGV = (StaggeredGridView) findViewById(R.id.grid);
-        mSGV.setColumnCount(4);
-        mSGV.setAdapter(mAdapter);
-        mSGV.setOnScrollListener(mScrollListener);
-        String url = "http://donglixia.sinaapp.com/app/service/";
+		// mAdapter = new SGVAdapter(this);
+		mAdapter = new DonglixiaAdapter(this, mImageFetcher, list);
+
+		mSGV = (StaggeredGridView) findViewById(R.id.grid);
+		mSGV.setColumnCount(4);
+		mSGV.setAdapter(mAdapter);
+		mSGV.setOnScrollListener(mScrollListener);
+		String url = "http://donglixia.sinaapp.com/app/service/";
 		Log.d("MainActivity", "current url:" + url);
 		DonglixiaTask task = new DonglixiaTask(this);
 		task.execute(url);
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		// getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    private final class SGVAdapter extends BaseAdapter
-    {
+	private final class SGVAdapter extends BaseAdapter {
 
-        LayoutInflater mInflater;
+		LayoutInflater mInflater;
 
-        public SGVAdapter(Context ctx)
-        {
-            mInflater = LayoutInflater.from(ctx);
-        }
+		public SGVAdapter(Context ctx) {
+			mInflater = LayoutInflater.from(ctx);
+		}
 
-        @Override
-        public int getCount()
-        {
-            return 30;
-        }
+		@Override
+		public int getCount() {
+			return 30;
+		}
 
-        @Override
-        public Object getItem(int position)
-        {
-            return null;
-        }
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
 
-        @Override
-        public long getItemId(int position)
-        {
-            return 0;
-        }
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
 
-        Random r = new Random();
+		Random r = new Random();
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent)
-        {
-            final LayoutParams lp;
-            final View v;
-            switch (position)
-            {
-                case 0:
-                case 29:
-                    v = mInflater.inflate(R.layout.element_header, parent, false);
-                    lp = new LayoutParams(v.getLayoutParams());
-                    lp.span = mSGV.getColumnCount();
-                    break;
-                case 8:
-                case 9:
-                case 18:
-                case 19:
-                    v = mInflater.inflate(R.layout.element_item_small, parent, false);
-                    lp = new LayoutParams(v.getLayoutParams());
-                    //lp.span = 1;
-                    break;
-                case 10:
-                case 20:
-                    v = mInflater.inflate(R.layout.element_item_large, parent, false);
-                    lp = new LayoutParams(v.getLayoutParams());
-                    lp.span = 4;
-                    break;
-                default:
-                    v = mInflater.inflate(R.layout.element_item, parent, false);
-                    lp = new LayoutParams(v.getLayoutParams());
-                    lp.span = 2;
-                    break;
-            }
-            v.setLayoutParams(lp);
-            return v;
-        }
-    }
-    class DonglixiaTask extends AsyncTask<String, Integer, List<Donglixia>> {
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final LayoutParams lp;
+			final View v;
+			switch (position) {
+			case 0:
+			case 29:
+				v = mInflater.inflate(R.layout.element_header, parent, false);
+				lp = new LayoutParams(v.getLayoutParams());
+				lp.span = mSGV.getColumnCount();
+				break;
+			case 8:
+			case 9:
+			case 18:
+			case 19:
+				v = mInflater.inflate(R.layout.element_item_small, parent, false);
+				lp = new LayoutParams(v.getLayoutParams());
+				// lp.span = 1;
+				break;
+			case 10:
+			case 20:
+				v = mInflater.inflate(R.layout.element_item_large, parent, false);
+				lp = new LayoutParams(v.getLayoutParams());
+				lp.span = 4;
+				break;
+			default:
+				v = mInflater.inflate(R.layout.element_item, parent, false);
+				lp = new LayoutParams(v.getLayoutParams());
+				lp.span = 2;
+				break;
+			}
+			v.setLayoutParams(lp);
+			return v;
+		}
+	}
 
-		private Context mContext;
+	class DonglixiaTask extends AsyncTask<String, Integer, List<Donglixia>> {
+
+		private final Context mContext;
 
 		public DonglixiaTask(Context context) {
 			super();
@@ -186,8 +179,8 @@ public class MainActivity extends Activity
 
 		@Override
 		protected void onPostExecute(List<Donglixia> result) {
-				list.addAll(result);
-				mAdapter.notifyDataSetChanged();
+			list.addAll(result);
+			mAdapter.notifyDataSetChanged();
 
 		}
 
@@ -202,10 +195,29 @@ public class MainActivity extends Activity
 				try {
 					json = Helper.getStringFromUrl(url);
 
+					json = json.substring(1, json.length());
+					Log.i("json:", json);
+					byte[] result = Base64.decode(json, Base64.DEFAULT);
+
+					json = new String(result);
+					Log.i("data:", json);
+					/*
+					 * String de1 = ss.substring(0, 9); System.out.println("s1:"
+					 * + de1);
+					 * 
+					 * String de2 = ss.substring(10, ss.length());
+					 * System.out.println("s2:" + de2);
+					 * 
+					 * String de = de1 + de2;
+					 * 
+					 * System.out.println(new String(Base64.decode(de)));
+					 */
 				} catch (IOException e) {
 					Log.e("IOException is : ", e.toString());
 					e.printStackTrace();
 					return duitangs;
+				} catch (Exception e) {
+
 				}
 			}
 			Log.d("MainActiivty", "json:" + json);
@@ -213,13 +225,14 @@ public class MainActivity extends Activity
 			try {
 				if (null != json) {
 					JSONObject newsObject = new JSONObject(json);
-					JSONObject jsonObject = newsObject.getJSONObject("data");
-					JSONArray blogsJson = jsonObject.getJSONArray("blogs");
+					// JSONObject jsonObject = newsObject.getJSONObject("data");
+					JSONArray dataJson = newsObject.getJSONArray("data");
 
-					for (int i = 0; i < blogsJson.length(); i++) {
-						JSONObject newsInfoLeftObject = blogsJson.getJSONObject(i);
+					for (int i = 0; i < dataJson.length(); i++) {
+						String urls = dataJson.getString(i);
 						Donglixia newsInfo1 = new Donglixia();
-						newsInfo1.setUrl(newsInfoLeftObject.isNull("albid") ? "" : newsInfoLeftObject.getString("albid"));
+						newsInfo1.setUrl(urls);
+						newsInfo1.setTag("美图");
 						duitangs.add(newsInfo1);
 					}
 				}
